42e492114b42ec6bac73f79c2d5c2654bbc8da47
Mmeso-iconic <mesougwuanyi@gmail.com>
2025-07-21 16:32:36 +0100

    Fixing Port issue for rendering

--- Backend/server.js
+++ Backend/server.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT_NUMBER || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
