6a53571eea9fceaa0940f06601fca493913058c1
Mmeso-iconic <mesougwuanyi@gmail.com>
2025-07-22 12:35:01 +0100

    Checkup

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,31 +1,26 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-// helper to create JWT
-const createToken = (_id) => {
-    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
-    };
 
-    // signup user
-    exports.signupUser = async (req, res) => {
+exports.signupUser = async (req, res) => {
     const { name, email, password } = req.body;
-    try {
-        const user = await User.signup(name, email, password);
-        const token = createToken(user._id);
-        res.status(200).json({ email: user.email, name: user.name, token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-    };
 
-    // login user
-    exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
     try {
-        const user = await User.login(email, password);
-        const token = createToken(user._id);
-        res.status(200).json({ email: user.email, name: user.name, token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+        const exists = await User.findOne({ email });
+        if (exists) return res.status(400).json({ error: 'Email already in use' });
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = await User.create({ name, email, password: hash });
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
     }
+};
+
+
+exports.loginUser = async (req, res) => {
+
 };
