b6a3a66870a6a3b1fda5fc4309b70a2e29fa2825
Mmeso-iconic <mesougwuanyi@gmail.com>
2025-07-22 13:11:28 +0100

    Login Logic

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,7 +2,6 @@ const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 exports.signupUser = async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -20,7 +19,21 @@ exports.signupUser = async (req, res) => {
     }
 };
 
-
 exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ error: 'Invalid email or password' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ error: 'Invalid email or password' });
+
+        // create token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ message: 'Login successful', token });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
