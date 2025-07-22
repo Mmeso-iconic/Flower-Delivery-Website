aeb82c5f97453558203f2aae20a22e5a6073cdc0
Mmeso-iconic <mesougwuanyi@gmail.com>
2025-07-22 14:00:46 +0100

    Commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,7 +2,6 @@ const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 exports.signupUser = async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -14,7 +13,10 @@ exports.signupUser = async (req, res) => {
         const hash = await bcrypt.hash(password, salt);
 
         const user = await User.create({ name, email, password: hash });
-        res.status(201).json(user);
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.status(201).json({ message: 'Signup successful', user, token });
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
@@ -30,10 +32,9 @@ exports.loginUser = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ error: 'Invalid email or password' });
 
-        // create token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.json({ message: 'Login successful', token });
+        res.json({ message: 'Login successful', user, token });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
