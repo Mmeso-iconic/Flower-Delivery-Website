b7c3549284e5f6c74714529b74e925bfa1e4921a
Mmeso-iconic <mesougwuanyi@gmail.com>
2025-09-10 17:19:53 +0100

    Route Check

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/users', userRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 // Catch-all for React Router
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
