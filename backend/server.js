fe3cdc7ac69f486744eda240d48c6bd1e843716b
Mmesoma Ugwuanyi <mesougwuanyi@gmail.com>
2025-11-07 14:29:54 +0100

    Updating server

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
