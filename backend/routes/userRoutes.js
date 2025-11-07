7a0c1a03532843f831d6bebb1e0721322fcd50d1
Mmesoma Ugwuanyi <mesougwuanyi@gmail.com>
2025-11-07 14:25:30 +0100

    profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
+const { protect } = require('../middlewares/protect');
 const express = require('express');
 const { signupUser, loginUser } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/signup', signupUser);
-
-
 router.post('/login', loginUser);
+
+router.get('/profile', protect, (req, res) => {
+  res.json({ message: `Welcome ${req.user.name}!`, user: req.user });
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,42 +2,42 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
+
 const flowerRoutes = require('./routes/flowerRoutes');
 const userRoutes = require('./routes/userRoutes');
-
-
-
 
 const app = express();
 
 
-app.use(cors());        
-app.use(express.json()); 
-app.use('/images', express.static(path.join(__dirname, 'uploads'))); 
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
+
+
 app.use('/api/flowers', flowerRoutes);
 app.use('/api/users', userRoutes);
 
-// Serve React static files
-app.use(express.static(path.join(__dirname, "public")));
 
-// Catch-all for React Router
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+  res.send('Flower API is running!');
 });
 
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-
-app.get('/', (req, res) => {
-    res.send('Flower API is running!');
-});
-
-
+  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
